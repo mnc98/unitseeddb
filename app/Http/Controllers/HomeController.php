784bca02963b7c4011bacd4f786daa47f34ff448
mnc98 <mncohen45@gmail.com>
2019-04-13 14:10:45 -0400

    Modified Controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,7 +30,7 @@ class HomeController extends Controller
     public function profile()
     {
         $user = $user = Auth::user();
-        dd($user->email);
+        dd($user);
         return view('profile', ['myvariable' => 'Hello World']);
     }
 }
