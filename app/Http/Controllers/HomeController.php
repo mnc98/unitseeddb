7517d1d7b2701026ecfe7447363e876c38a6627d
mnc98 <mncohen45@gmail.com>
2019-04-13 14:25:06 -0400

    Added Procfile

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -30,7 +31,7 @@ class HomeController extends Controller
     public function profile()
     {
         $user = $user = Auth::user();
-        dd($user);
+        dd($user->email);
         return view('profile', ['myvariable' => 'Hello World']);
     }
 }
